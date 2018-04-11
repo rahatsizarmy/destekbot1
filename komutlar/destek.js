1b389bb73aa36f3126348419313a5babfa41ef88
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-04-11 19:16:55 +0300

    Update destek.js

--- komutlar/destek.js
+++ komutlar/destek.js
@@ -3,18 +3,22 @@ const moment = require("moment");
 require("moment-duration-format");
 
 exports.run = (client, message, args) => {
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`destek` adlı komutu özel mesajlarda kullanamazsın.') } 
+    if (!message.guild) {
+    const embed = new Discord.RichEmbed() 
+
+    .addField('Merak Etme Yakında Destke Ekibimiz Burada')
+    
+    .setColor(0xff0000)
+
+    return message.channel.sendEmbed(embed)
+    return message.author.sendEmbed(ozelmesajuyari); } 
     let guild = message.guild 
     let reason = args.slice(1).join(' ');
     let user = message.mentions.users.first();
     let modlog = guild.channels.find('id', '433638952629436417');
     if (reason.length < 1) return message.reply('Destek Talebini  Yazmalısın');
-    if (message.mentions.users.size < 1) return message.reply('**Beni Etiketlemen Lavım!').catch(console.error);
-  
+    if (message.mentions.users.size < 1) return message.reply('Beni Etiketlemelisin Örnek Kullanım: `,destek @Destek Bot konu`').catch(console.error);
+
     guild.channels.get(modlog.id).send("@here **Yeni Destek Talebi Var!**") 
     const embed = new Discord.RichEmbed()
       .setColor(0x00AE86)
